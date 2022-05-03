5bff1e28f99f9d7c9a2f37b43863f6bfc14dc844
Denis_FPCUSER <nonames@bk.ru>
2022-05-03 17:55:45 +0300

    add qr_read_data

--- main.py
+++ main.py
@@ -1,60 +1,20 @@
-# import modules
-import qrcode
 from PIL import Image
 import pyqrcode
-# import png
+import cv2
 
 
-def pic_in_qr_1():
-    # taking image which user wants
-    # in the QR code center
-    Logo_link = 'manchester_united_PNG26.png'
-
-    logo = Image.open(Logo_link)
-
-    # taking base width
-    basewidth = 50
-
-    # adjust image size
-    wpercent = (basewidth / float(logo.size[0]))
-    hsize = int((float(logo.size[1]) * float(wpercent)))
-    # logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
-    logo = logo.resize((basewidth, hsize), Image.Resampling.LANCZOS)
-    QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
-
-    # taking url or text
-    url = '123456789'
-
-    # adding URL or text to QRcode
-    QRcode.add_data(url)
-
-    # generating QR code
-    QRcode.make()
-
-    # taking color name from user
-    QRcolor = 'Green'
-
-    # adding color to QR code
-    QRimg = QRcode.make_image(
-        fill_color=QRcolor, back_color="white").convert('RGB')
-
-    # set size of QR code
-    pos = ((QRimg.size[0] - logo.size[0]) // 2,
-           (QRimg.size[1] - logo.size[1]) // 2)
-    QRimg.paste(logo, pos)
-
-    # save the QR code generated
-    QRimg.save('gfg_QR.png')
-
-    print('QR code generated!')
-
-
-def pic_in_qr_2(text, logo=None):
+def qr_generator(text, logo=None):
+    """
+    Функция генерирует QR-код. \n
+    Также на вход можно подать логотип в .png формате, который будет размещен в центре QR-кода. \n
+    :param text: текст для преобразования в QR-код
+    :param logo: путь к логотипу
+    """
     # url = pyqrcode.QRCode('http://www.eqxiu.com', error='H')
-    url = pyqrcode.create(text, error='H', version=2)
+    url = pyqrcode.create(text, error='H')
     url.png('test.png', scale=20,
-            module_color=[250, 128, 0, 0],
-            background=[0xcf, 0xbf, 0xcf],
+            # module_color=[250, 128, 0, 0],
+            # background=[0xcf, 0xbf, 0xcf],
             quiet_zone=2)
 
     if logo:
@@ -65,13 +25,21 @@ def pic_in_qr_2(text, logo=None):
         logo = Image.open(logo)
         xmin = ymin = int((width / 2) - (logo_size / 2))
         xmax = ymax = int((width / 2) + (logo_size / 2))
-        region = logo
-        region = region.resize((xmax - xmin, ymax - ymin))
-        im.paste(region, (xmin, ymin, xmax, ymax), region)
+        logo = logo.resize((xmax - xmin, ymax - ymin))
+        im.paste(logo, (xmin, ymin, xmax, ymax), logo)
         im.save('test.png')
+
+
+def qr_read_data():
+    img = cv2.imread("test.png")
+    detector = cv2.QRCodeDetector()
+    data, bbox, straight_qrcode = detector.detectAndDecode(img)
+    return data
 
 
 if __name__ == '__main__':
     logo = 'manchester_united_PNG26.png'
-    text = '12494054524'
-    pic_in_qr_2(text, logo=logo)
+    text = 'simple text'
+    qr_generator(text, logo=logo)
+    data = qr_read_data()
+    print(data)
